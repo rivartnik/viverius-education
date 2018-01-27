d67722e428d85fc033a23ae18598131c7e7201f3
Rok <rokivartnik@gmail.com>
2018-01-27 17:16:15 +0100

    27.1.2018

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		<![endif]-->
   </head>
   <body>
-    <!-- Page-->
+    <!-- Sliders-->
     <div class="page text-center">
       <div class="page-loader page-loader-variant-1">
         <div><img class='img-responsive' style='margin-top: -20px;margin-left: -18px;' width='330' height='67' src='images/logos/viverius.png' alt=''/>
@@ -33,7 +33,7 @@
           <!--Swiper-->
     <div class="swiper-container swiper-slider" data-loop="true" data-autoplay="true" data-height="100vh" data-dragable="false" data-min-height="480px">
       <div class="swiper-wrapper text-center">
-        <div class="swiper-slide" id="page-loader" data-slide-bg="images/sliders/slider6.jpeg" data-preview-bg="images/sliders/slider6.jpeg">
+        <div class="swiper-slide" id="page-loader" data-slide-bg="images/sliders/slider1.jpg" data-preview-bg="images/sliders/slider1.jpg">
           <div class="swiper-caption swiper-parallax">
             <div class="swiper-slide-caption">
               <div class="shell">
@@ -46,34 +46,33 @@
                       The smartest and most flexible bootstrap template by TemplateMonster you've ever seen.
                       Create exactly what you need with our powerful bootstrap toolkit.
                     </h4>
-                   <!-- <div class="group group-xl offset-top-20 offset-xs-top-50"><a class="btn btn-primary btn-lg btn-anis-effect" href="//ld-wp2.template-help.com/novi-builder/intense-builder/" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Try builder now</span></a><a class="btn btn-default btn-lg btn-anis-effect" href="//www.templatemonster.com/intense-multipurpose-html-template.html?utm_source=livedemo&amp;utm_medium=themebutton&amp;utm_campaign=intense" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Buy intense now</span></a></div>-->
+                   <div class="group group-xl offset-top-20 offset-xs-top-50"><a class="btn btn-primary btn-lg btn-anis-effect" href="//ld-wp2.template-help.com/novi-builder/intense-builder/" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Preveri naše storitve</span></a><a class="btn btn-default btn-lg btn-anis-effect" href="//www.templatemonster.com/intense-multipurpose-html-template.html?utm_source=livedemo&amp;utm_medium=themebutton&amp;utm_campaign=intense" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Prijava na tečaj</span></a></div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="swiper-slide" data-slide-bg="images/sliders/slider7.jpg" data-preview-bg="images/sliders/slider7.jpg">
+        <div class="swiper-slide" data-slide-bg="images/sliders/slider2.jpg" data-preview-bg="images/sliders/slider2.jpg">
           <div class="swiper-caption swiper-parallax">
             <div class="swiper-slide-caption">
               <div class="shell">
                 <div class="range range-lg-center">
                   <div class="cell-lg-12">
-                    <h1><span class="big text-uppercase" data-caption-animate="fadeIn" data-caption-delay="700">Kreditne točke Zdravniške zbornice Slovenije</span></h1>
+                    <h1><span class="big text-uppercase" data-caption-animate="fadeIn" data-caption-delay="700">Online klinični primeri</span></h1>
                   </div>
                   <div class="cell-lg-10 offset-top-30">
                     <h4 class="hidden reveal-sm-block text-light offset-bottom-0" data-caption-animate="fadeIn" data-caption-delay="900">
-                      Beautiful and clean designs are optimized for all screen sizes
-                      and types. Taste a new meaning of Retina Ready.
+                      Naš najpopularnejši tečaj ima razpisane nove tečaje!
                     </h4>
-                    <div class="group group-xl offset-top-20 offset-xs-top-50"><a class="btn btn-icon btn-icon-left btn-primary btn-lg btn-anis-effect" href="//www.templatemonster.com/intense-multipurpose-html-template.html?utm_source=livedemo&amp;utm_medium=themebutton&amp;utm_campaign=intense" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Preberi več</span></a></div>
+                    <div class="group group-xl offset-top-20 offset-xs-top-50"><a class="btn btn-icon btn-icon-left btn-primary btn-lg btn-anis-effect" href="//www.templatemonster.com/intense-multipurpose-html-template.html?utm_source=livedemo&amp;utm_medium=themebutton&amp;utm_campaign=intense" data-caption-animate="fadeIn" data-caption-delay="1200"><span class="btn-text">Preveri tečaje</span></a></div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="swiper-slide" data-slide-bg="images/sliders/slider8.jpg" data-preview-bg="images/sliders/slider8.jpg">
+        <div class="swiper-slide" data-slide-bg="images/sliders/slider3.jpg" data-preview-bg="images/sliders/slider3.jpg">
           <div class="swiper-caption swiper-parallax">
             <div class="swiper-slide-caption">
               <div class="shell">
@@ -112,6 +111,243 @@
     </div>
       <!-- Page Contents-->
       <main class="page-content">
+      <section class="offset-top-50 offset-top-lg-70 text-center">
+      <div class="shell">
+        <h1>Dobrodošli</h1>
+        <hr class="divider bg-mantis">
+        <div class="offset-top-66">
+          <div class="reveal-inline-block reveal-lg-block">
+            <div class="range range-sm-bottom range-xs-center">
+              <div class="cell-xs-8 cell-lg-12">
+                <div class="range">
+                  <div class="cell-sm-6 cell-lg-4">
+                    <!-- Counter type 3-->
+                    <div class="counter-type-3">
+                      <div class="unit unit-horizontal unit-align-center unit-spacing-xs">
+                        <div class="unit-left"><span class="icon mdi mdi-vector-arrange-below text-mantis"></span></div>
+                        <div class="unit-body text-left">
+                          <div>
+                          <div><span class="h1 text-bold counter" data-speed="2500" data-from="0" data-to="60"></span><span class="h1 text-bold">+</span>
+                          </div>
+                          </div>
+                          <div>
+                            <h6 class="text-uppercase text-bold text-dark">Zaključenih tečajev</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-sm-6 cell-lg-4">
+                    <!-- Counter type 3-->
+                    <div class="counter-type-3">
+                    <div class="unit unit-horizontal unit-align-center unit-spacing-xs">
+                        <div class="unit-left"><span class="icon mdi mdi-account-multiple-outline text-malibu"></span></div>
+                        <div class="unit-body text-left">
+                          <div><span class="h1 text-bold counter" data-speed="2500" data-from="0" data-to="130"></span><span class="h1 text-bold">+</span>
+                          </div>
+                          <div>
+                            <h6 class="text-uppercase text-bold text-dark">Udeležencev tečajev</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-sm-6 cell-lg-4">
+                    <!-- Counter type 3-->
+                    <div class="counter-type-3">
+                      <div class="unit unit-horizontal unit-align-center unit-spacing-xs">
+                        <div class="unit-left"><span class="icon mdi mdi-emoticon-happy"></span></div>
+                        <div class="unit-body text-left">
+                          <div>
+                          <div><span class="h1 text-bold counter" data-speed="2500" data-from="0" data-to="97"></span><span class="h1 text-bold">%</span>
+                          </div>
+                          </div>
+                          <div>
+                            <h6 class="text-uppercase text-bold text-dark">Zadovoljih tečajnikov</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>       
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="shell offset-top-66 text-left">
+        <div class="code-counter-type-3"></div>
+      </div>
+    </section>
+    <!-- Offers-->
+    <section class="section-34">
+    <div class="shell-fluid">
+      <div class="range range-xs-center">
+        <div class="cell-xs-8 cell-sm-6 cell-lg-3">
+          <!-- Thumbnail Terry-->
+          <figure class="thumbnail-terry"><a href="#"><img width="442" height="280" src="images/index_storitve/okp.jpg" alt=""/></a>
+            <figcaption>
+              <div>
+                <h4 class="thumbnail-terry-title">Online klinični primeri</h4>
+              </div>
+           <a class="btn offset-top-10 offset-md-top-0 btn-primary" href="#">Več o</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="cell-xs-8 cell-sm-6 cell-lg-3 offset-top-30 offset-sm-top-0">
+          <!-- Thumbnail Terry-->
+          <figure class="thumbnail-terry"><a href="#"><img width="442" height="280" src="images/index_storitve/opsi.jpg" alt=""/></a>
+            <figcaption>
+              <div>
+                <h4 class="thumbnail-terry-title">Online priprave na strokovni izpit</h4>
+              </div>
+              <a class="btn offset-top-10 offset-md-top-0 btn-primary" href="#">Več o</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="cell-xs-8 cell-sm-6 cell-lg-3 offset-top-30 offset-lg-top-0">
+          <!-- Thumbnail Terry-->
+          <figure class="thumbnail-terry"><a href="#"><img width="442" height="280" src="images/index_storitve/odsm.jpg" alt=""/></a>
+            <figcaption>
+              <div>
+                <h4 class="thumbnail-terry-title">Online delavnice za<br> študente medicine</h4>
+              </div>
+             <a class="btn offset-top-10 offset-md-top-0 btn-primary" href="#">Več o</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="cell-xs-8 cell-sm-6 cell-lg-3 offset-top-30 offset-lg-top-0">
+          <!-- Thumbnail Terry-->
+          <figure class="thumbnail-terry"><a href="#"><img width="442" height="280" src="images/index_storitve/osk.jpg" alt=""/></a>
+            <figcaption>
+              <div>
+                <h4 class="thumbnail-terry-title">Online seminarji in kongresi</h4>
+              </div>
+             <a class="btn offset-top-10 offset-md-top-0 btn-primary" href="#">Več o</a>
+            </figcaption>
+          </figure>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+   <!-- Bootstrap Pills (Stacked)-->
+   <section class="offset-top-20 offset-md-top-40">
+          <div class="shell">
+            <h1>O nas</h1>
+            <hr class="divider bg-mantis">
+            <div class="offset-sm-top-50 text-left">
+              <!-- Bootstrap Tabs-->
+              <div class="nav-stacked-container">
+                <ul class="tabs nav nav-pills nav-stacked text-center text-md-left" role="tablist" id="tabs-3">
+                  <li class="active" role="presentation"><a href="#tab001" role="tab">Kdo smo</a></li>
+                  <li role="presentation"><a href="#tab002" role="tab">Kaj delamo</a></li>
+                  <li role="presentation"><a href="#tab003" role="tab">Naši tečaji</a></li>
+                </ul>
+                <div class="tab-content tab-conten-vert offset-top-30 text-md-left">
+                  <div class="tab-pane fade active in" role="tabpanel" id="tab001"><p><span class="text-bold big">Our professional services are the keys to your success</span><p>Nowadays, almost everyone has a website.Basically, this is a business card that you present every visitor or partner with. So, the better it is designed the more likely you can expect your further cooperation. The content on your website is important as well.    </p> <p> The information should be not only well-organized and easy to read, but it should be also presented in a visually pleasing manner, thus making the process of reading an enjoyable journey. Adorn posts with bold imagery, and users will get more interested and involved.</p></div>
+                  <div class="tab-pane fade" role="tabpanel" id="tab002"><p>Nowadays, almost everyone has a website.Basically, this is a business card that you present every visitor or partner with. So, the better it is designed the more likely you can expect your further cooperation. The content on your website is important as well. </p> <p> The information should be not only well-organized and easy to read, but it should be also presented in a visually pleasing manner, thus making the process of reading an enjoyable journey. Adorn posts with bold imagery, and users will get more interested and involved.</p></div>
+                  <div class="tab-pane fade" role="tabpanel" id="tab003"><p>Nowadays, almost everyone has a website.Basically, this is a business card that you present every visitor or partner with. So, the better it is designed the more likely you can expect your further cooperation. The content on your website is important as well. </p> <p> The information should be not only well-organized and easy to read, but it should be also presented in a visually pleasing manner, thus making the process of reading an enjoyable journey. Adorn posts with bold imagery, and users will get more interested and involved.</p></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="shell offset-top-66 text-left">
+        <div class="code-counter-type-3"></div>
+      </div>
+        </section>
+
+
+  
+<!-- Join Our Newsletter-->
+    <section class="section parallax-container context-dark" data-parallax-img="images/bg-03-1920x650.jpg">
+          <div class="parallax-content">
+            <div class="shell section-110">
+              <div class="range range-xs-center">
+                <div class="cell-xs-10 cell-sm-7 cell-lg-5">
+                  <div>
+                    <h2 class="text-bold">Viverius novice</h2>
+                  </div>
+                  <div class="offset-top-17">
+                    <p>Bodite obveščeni o najnovejših tečajih in novostih.<br class="veil reveal md-inline-block"> Vpišite svoj email in se prijavite na naše novice.</p>
+                  </div>
+                  <div class="offset-top-30 offset-md-top-60 inset-md-left-55 inset-md-right-55">
+                    <form class="rd-mailform" data-form-output="form-subscribe-footer" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
+                      <div class="form-group">
+                        <div class="input-group input-group-sm"><span class="input-group-addon"><span class="input-group-icon mdi mdi-email"></span></span>
+                          <input class="form-control" placeholder="Enter your E-Mail" type="email" name="email" data-constraints="@Email @Required"/><span class="input-group-btn">
+                            <button class="btn btn-sm btn-primary" type="submit">Subscribe</button></span>
+                        </div>
+                      </div>
+                      <div class="form-output" id="form-subscribe-footer"></div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+       
+        <section class="section-80 section-md-110 section-md-bottom-150 bg-white">
+        <div class="shell">
+          <h2 class="text-bold">Mnenja naših tečajnikov</h2>
+          <div class="range range-xs-center offset-md-top-30 offset-top-60">
+            <div class="cell-sm-8 cell-md-4 wow fadeInLeft" data-wow-delay="0.3s">
+              <blockquote class="quote quote-classic quote-outboxed">
+                <div class="quote-body">
+                  <p class="text-darker">
+                    <q>Delavnice so inovativen in sodoben način izobraževanja. Veliko lahko prispevajo k praktičnemu znanju udeležencev, prihranijo čas, saj zanje ni potrebno oditi od doma, hkrati pa ponujajo realen vpogled v trenutno zdravniško prakso na primarnem nivoju.</q>
+                  </p>
+                </div>
+                <div class="quote-meta unit unit-horizontal unit-spacing-sm unit-middle">
+                  <div class="unit-body unit-item-wide">
+                    <h6 class="quote-author text-uppercase">
+                      <cite class="text-normal">Zala Mravljak</cite>
+                    </h6>
+                    <p class="quote-desc text-uppercase text-primary">dr. med. specizalizantka družinske medicine</p>
+                  </div>
+                </div>
+              </blockquote>
+            </div>
+            <div class="cell-sm-8 cell-md-4 offset-top-60 offset-md-top-0 wow fadeInLeft" data-wow-delay="0.5s">
+              <blockquote class="quote quote-classic quote-outboxed">
+                <div class="quote-body">
+                  <p class="text-darker">
+                    <q>Odlična priložnost preiskusa svojega znanja, nadaljnega razvoja kritičnega razmišljanja in možnost diskusije z ostalimi udeleženci in izkušenim klinikom v sproščenem in zabavnem okolju.</q>
+                  </p>
+                </div>
+                <div class="quote-meta unit unit-horizontal unit-spacing-sm unit-middle">
+                  <div class="unit-body unit-item-wide">
+                    <h6 class="quote-author text-uppercase">
+                      <cite class="text-normal">Denis Sraka</cite>
+                    </h6>
+                    <p class="quote-desc text-uppercase text-primary">dr.med.</p>
+                  </div>
+                </div>
+              </blockquote>
+            </div>
+            <div class="cell-sm-8 cell-md-4 offset-top-60 offset-md-top-0 wow fadeInLeft" data-wow-delay="0.7s">
+              <blockquote class="quote quote-classic quote-outboxed">
+                <div class="quote-body">
+                  <p class="text-darker">
+                    <q>Delavnico najlažje opišem kot prizor iz serije Urgenca ali House MD, kjer v skupini zdravnikov z izkušenim mentorjem poteka obravnava kliničnih primerov pacientov. Pripravljene so časovne in druge omejitve, ki udeležencu ustvarijo občutek realne udeležbe pri reševanju.</q>
+                  </p>
+                </div>
+                <div class="quote-meta unit unit-horizontal unit-spacing-sm unit-middle">
+                  <div class="unit-body unit-item-wide">
+                    <h6 class="quote-author text-uppercase">
+                      <cite class="text-normal">Sara Muren</cite>
+                    </h6>
+                    <p class="quote-desc text-uppercase text-primary">dr.med.</p>
+                  </div>
+                </div>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </section>
+
 
       </main>
       <!-- footer-->
