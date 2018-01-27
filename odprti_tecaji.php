51b3133a7520094c97d3ac01518b1787a636f5b3
Rok <rokivartnik@gmail.com>
2018-01-27 23:31:15 +0100

    final 27.1.2018

---
+++ odprti_tecaji.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html class="wide wow-animation scrollTo smoothscroll" lang="en">
+  <head>
+    <!-- Site Title-->
+    <title>Online klinični primeri</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="intense web design multipurpose template html">
+    <meta name="date" content="Dec 26">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,700%7CLato:300,300italic,400,700,900%7CYesteryear">
+    <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+    <!-- Page-->
+    <div class="page text-center">
+      <!-- Page Header-->
+      <?php include("header.html");?>
+      <!-- Classic Breadcrumbs-->
+      <section class="breadcrumb-classic">
+        <div class="shell section-34 section-sm-50">
+          <div class="range range-lg-middle">
+            <div class="cell-lg-5 veil reveal-lg-block cell-lg-push-1 text-lg-left">
+              <h3><span class="big">Odprti tečaji</span></h3>
+            </div>
+            <div class="offset-top-0 offset-sm-top-10 cell-lg-5 offset-lg-top-0 small cell-lg-push-3 text-lg-right">
+              <ul class="list-inline list-inline-dashed p">
+                <li><a href="index.php">Domov</a></li>
+                <li>Odprti tečaji
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Page Content-->
+      <main class="page-content section-top-20 section-sm-top-50">
+
+    <div class="parallax-container" data-parallax-img="images/backgrounds/background-05-1920x960.jpg">
+        <div class="parallax-content">
+        <section class="bg-overlay-white" id="rd-event-calendar-demo">
+          <div class="shell-wide section-50 section-sm-50">
+            <h4>Preverite naše razpisane tečaje. Če se želite prijaviti preprosti kliknite na želen datum in izberite opcijo prijava. Odprlo se vam bo novo okno, kjer vnesete vaše podatke.</h4>
+            <hr class="divider bg-mantis">
+            <div class="offset-top-20">
+              <div class="rd-calendar-fullwidth">
+                        <div class="rd-calendar">
+                          <div class="rdc-panel"><a class="rdc-next"></a><a class="rdc-prev"></a>
+                            <div class="rdc-month"></div>
+                            <div class="rdc-fullyear"></div>
+                          </div>
+                          <div class="rdc-table"></div>
+                          <div class="rdc-events text-left"><a class="rdc-events_close"></a>
+                            <ul>
+                              <li class="rdc-event" data-date="01/01/2018">
+                                <div class="rdc-event-wrap bg-blue-gray context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">10:00 AM</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">International Conference</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="01/02/2018">
+                                <div class="rdc-event-wrap bg-blue-gray context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">10:00 am</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">International Conference Day 2</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="04/19/2016">
+                                <div class="rdc-event-wrap bg-mantis context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">9:00 AM</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Opening of the exhibition</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="04/28/2016">
+                                <div class="rdc-event-wrap bg-mantis context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">6:00 pm</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Exhibition closing</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="04/29/2016">
+                                <div class="rdc-event-wrap bg-saffron context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">9:00 AM</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Family breakfast</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/05/2016">
+                                <div class="rdc-event-wrap bg-blue-gray context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">10:00 am</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">International Conference</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/16/2016">
+                                <div class="rdc-event-wrap bg-blue-gray context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">10:00 am</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">International Conference</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/17/2016">
+                                <div class="rdc-event-wrap bg-blue-gray context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">10:00 am</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">International Conference Day 2</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/19/2016">
+                                <div class="rdc-event-wrap bg-mantis context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">9:00 AM</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Opening of the exhibition</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/28/2016">
+                                <div class="rdc-event-wrap bg-mantis context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">6:00 pm</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Exhibition closing</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                              <li class="rdc-event" data-date="05/23/2016">
+                                <div class="rdc-event-wrap bg-saffron context-dark">
+                                  <article class="post widget-event">
+                                    <div class="post-meta"><span class="icon icon-xxs text-picton-blue mdi mdi-clock"></span>
+                                      <time class="text-dark" datetime="2016-01-01">9:00 AM</time>
+                                    </div>
+                                    <div class="post-title">
+                                      <h6 class="text-regular"><a href="#">Family breakfast</a></h6>
+                                    </div>
+                                  </article>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+              </div>
+            </div>
+          </div>
+      
+        </section>
+        </div>
+    </div>
+      </main>
+
+
+      <!-- Page Footers-->
+      <!-- Default footer-->
+      <?php include("footer.html");?>
+    </div>
+    <!-- JavaScript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
